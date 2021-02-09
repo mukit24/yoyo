2ac541e1dd7d9352e8438f64d8ec6d3995b0e1e1
mukit hasan pranto <mukithasanpranto4@gmail.com>
2021-02-09 11:12:08 +0600

    whitenoise

--- lets_save_our_environment/settings.py
+++ lets_save_our_environment/settings.py
@@ -50,6 +50,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
